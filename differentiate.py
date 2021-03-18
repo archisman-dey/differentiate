8ef359dbe8a846f1ad51eb137ec20586832708fa
Archisman Dey <archismandey13@gmail.com>
2021-03-18 21:25:31 +0530

    removed a print statement

--- differentiate.py
+++ differentiate.py
@@ -6,9 +6,6 @@ print(*Globals.unary_operators, sep=', ', end = '\n\n')
 
 print("Use x, y, or z as variables.", end = '\n\n')
 
-print("Also, if there is a digit just after '-', it is always interpreted as a negative number, "
-    "so something like '2-1' should be written as '2 - 1'", end = '\n\n')
-
 input_string = input("Differentiate: ")
 var = Var(input("with respect to: ").strip())
 expression: AbstractExpression = Parser(input_string).parse()
